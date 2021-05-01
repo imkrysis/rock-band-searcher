5d62767ee1955e61611f9cc07a9db39da205ac06
imkrysis <elkrysss@gmail.com>
2021-05-01 20:12:07 +0200

    List and button Search added, when the button is clicked, API is called and returns a list of artists according to query

--- www/js/init.js
+++ www/js/init.js
@@ -1,26 +1,50 @@
-document.addEventListener('deviceready', onDeviceReady, false)
- 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
- 
-  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
-  document.getElementById('deviceready').classList.add('ready')
-
-  tabs = document.getElementById('tabs-swipe-demo')
-  tabsInstance = M.Tabs.getInstance(tabs)
-  tabsInstance.select('#tab3')
-
-}
-
 (function($){
   $(function(){
 
     $('#sidenav').sidenav()
     $('#tabs').tabs({'swipeable': true})
+    $('#btnSearch').click(getArtists)
+    
 
   }) // end of document ready
 })(jQuery) // end of jQuery name space
 
+const apiUrl = "https://musicbrainz.org/ws/2/"
+
+function getArtists(){
+
+  query = "artist?query=" + $('#inputSearch').val()
+
+	$.ajax({
+	  method: "GET",
+	  url: apiUrl + query,
+	  dataType: "json",
+    
+	}).done(function (msg) {
+    
+		$('#listResult').empty();
+
+	  for(i in msg.artists) {
+
+      if (msg.artists[i].disambiguation  == null) {
+
+        $('#listResult').append('<li><h5>'+msg.artists[i].name+'</h5>'+'<a href="#!" class="secondary-content"></a></li>');
+
+      } else {
+
+        $('#listResult').append('<li><h5>'+msg.artists[i].name+'</h5>'+'('+msg.artists[i].disambiguation +')<a href="#!" class="secondary-content"></a></li>');
+
+      }	   	
+
+	  };
+
+	}).fail(function () {
+
+		alert("There has been an unexpected problem");
+
+	});
+
+}
 
 
 
