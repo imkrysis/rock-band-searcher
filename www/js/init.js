102355a28f59df468f365fc9560b2b4fea2bfd4b
imkrysis <elkrysss@gmail.com>
2021-05-01 18:56:35 +0200

    Sidenav/hamburger menu when the screen/window is small enough

--- www/js/init.js
+++ www/js/init.js
@@ -1,22 +1,26 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav()
-    $('#tabs').tabs({"swipeable": true })
-
-  }) // end of document ready
-})(jQuery) // end of jQuery name space
-
 document.addEventListener('deviceready', onDeviceReady, false)
  
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
  
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
-    document.getElementById('deviceready').classList.add('ready')
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
+  document.getElementById('deviceready').classList.add('ready')
 
-    
+  tabs = document.getElementById('tabs-swipe-demo')
+  tabsInstance = M.Tabs.getInstance(tabs)
+  tabsInstance.select('#tab3')
 
 }
+
+(function($){
+  $(function(){
+
+    $('#sidenav').sidenav()
+    $('#tabs').tabs({'swipeable': true})
+
+  }) // end of document ready
+})(jQuery) // end of jQuery name space
+
+
 
 
